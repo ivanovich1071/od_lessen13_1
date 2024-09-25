213a9e7d6e3c187eda9a05e62b8275660c0a3c94
ivanovich1071 <pts.bryansk@mail.ru>
2024-09-25 16:09:28 +0300

    add main_graf

---
+++ main_graf.py
@@ -0,0 +1,25 @@
+class Graph:
+    def __init__(self):
+        self.graph = {}
+
+    def add_edge(self, u, v):
+        if u not in self.graph:
+            self.graph[u] = []
+        self.graph[u].append(v)
+
+    def print_graph(self):
+        for node in self.graph:
+            print(node, "->", " -> ".join(map(str, self.graph[node])))
+
+
+g = Graph()
+g.add_edge(0, 1)
+g.add_edge(0, 4)
+g.add_edge(1, 2)
+g.add_edge(1, 3)
+g.add_edge(1, 4)
+g.add_edge(2, 3)
+g.add_edge(3, 4)
+
+# Печатаем граф
+g.print_graph()
